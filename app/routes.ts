7505c7b129f270dbc636b262b5923b1c98954a29
Abhishek V <abhishek.v@incubyte.co>
2025-09-24 12:01:22 +0530

    Fixed home route

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,8 @@ import { type RouteConfig, index } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
 export default [
-  index("routes/home.tsx"),
+  index("routes/dashboard.tsx"),
   ...(await flatRoutes({
-    ignoredRouteFiles: ["routes/home.tsx"],
+    ignoredRouteFiles: ["routes/dashboard.tsx"],
   })),
 ] satisfies RouteConfig;
